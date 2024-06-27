941b515b55695234a7f9edde6990e447a635abd3
natalyemedina <natalyemedina3@outlook.com>
2024-06-27 12:10:42 -0500

    Completed Task 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,32 +11,45 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  "Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "
+];
+let correctAnswers = [
+  "Sally Ride",
+  "true",
+  "40",
+  "Trajectory",
+  "3"
+];
+let candidateAnswers = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  candidateName = input.question("Enter you name: ");
+  candidateName = input.question("Candidate name: ");
 }
-//<I added this line with "//" just to push it again because I needed to make a change to push it after giving permission to action on github
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question);
-
+  for (let i = 0; i < questions.length; i++) {
+   candidateAnswers[i] = input.question(questions[i]);
+  }
 }
+
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (correctAnswer === candidateAnswer) {
-  console.log("Correct answer");
-} else {
-  console.log("Incorrect answer.");
-} 
-
-
+  for (let i = 0; i < questions.length; i++) {
+  if (correctAnswers[i].toLowerCase === candidateAnswers[i].toLowerCase) {
+  console.log(`${i+1}) ${questions[i]}`);
+  console.log(`Your Answer: ${candidateAnswers[i]}`);
+  console.log(`Correct Answer: ${correctAnswers[i]}`);
+  }
+}
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
 
